81dd8bbdbab36430deda9e456fd21bbc999c175c
Vitor Tavares <vitortavares.o55@gmail.com>
2024-07-13 12:01:39 -0300

    Created a Dialog Box to copy the github @, linkedin and wpp

--- script.js
+++ script.js
@@ -80,3 +80,102 @@ window.addEventListener('load', function () {
     });
     status.innerText = "";
 });
+
+document.addEventListener('click', clickRemove);
+
+function clickRemove(event) {
+    let isClickInsideCard = false;
+    const cards = document.querySelectorAll('.card-copy');
+    cards.forEach(card => {
+        if (card.contains(event.target)) {
+            isClickInsideCard = true;
+        }
+    });
+
+    if (!isClickInsideCard) {
+        cards.forEach(card => card.remove());
+    }
+}
+
+function addCopyOnClick(inputElement) {
+    inputElement.addEventListener('click', function (event) {
+        navigator.clipboard.writeText(inputElement.value);
+
+        const tooltip = document.createElement('span');
+        tooltip.innerText = 'Copiado!';
+        tooltip.style.position = 'absolute';
+        tooltip.style.zIndex = '2';
+        tooltip.style.background = 'black';
+        tooltip.style.color = 'white';
+        tooltip.style.padding = '5px';
+        tooltip.style.borderRadius = '5px';
+        tooltip.style.fontSize = '12px';
+        tooltip.style.top = `${event.clientY + 170}px`;
+        tooltip.style.left = `${event.clientX}px`;
+
+        document.body.appendChild(tooltip);
+
+        setTimeout(() => {
+            tooltip.remove();
+        }, 2000);
+    });
+}
+
+function copyContent(event) {
+    event.stopPropagation();
+
+    const existingCard = document.querySelector('.card-copy');
+    if (existingCard) {
+        existingCard.remove();
+    }
+
+    const card = document.createElement('div');
+    card.className = 'card-copy';
+    card.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    const containerGithub = document.createElement('div');
+    const iconGithub = document.createElement('i');
+    iconGithub.className = 'devicon-github-plain icon-input-copy';
+    const inputGithub = document.createElement('input');
+    inputGithub.value = document.getElementById('github').dataset.value;
+    inputGithub.setAttribute('readonly', true);
+    inputGithub.className = 'input-read-only';
+    containerGithub.appendChild(iconGithub);
+    containerGithub.appendChild(inputGithub);
+    addCopyOnClick(inputGithub);
+
+    const containerLinkedin = document.createElement('div');
+    const iconLinkedin = document.createElement('i');
+    iconLinkedin.className = 'devicon-linkedin-plain icon-input-copy';
+
+    const inputLinkedin = document.createElement('input');
+    inputLinkedin.value = document.getElementById('linkedin').dataset.value;
+    inputLinkedin.setAttribute('readonly', true);
+    inputLinkedin.className = 'input-read-only';
+    containerLinkedin.appendChild(iconLinkedin);
+    containerLinkedin.appendChild(inputLinkedin);
+    addCopyOnClick(inputLinkedin);
+
+    const containerWpp = document.createElement('div');
+    const iconWpp = document.createElement('i');
+    iconWpp.className = 'fi fi-brands-whatsapp icon-input-copy';
+    const inputWpp = document.createElement('input');
+    inputWpp.value = document.getElementById('wpp').dataset.value;
+    inputWpp.setAttribute('readonly', true);
+    inputWpp.className = 'input-read-only';
+    containerWpp.appendChild(iconWpp);
+    containerWpp.appendChild(inputWpp);
+    addCopyOnClick(inputWpp);
+
+    card.appendChild(containerGithub);
+    card.appendChild(containerLinkedin);
+    card.appendChild(containerWpp);
+
+    document.body.appendChild(card);
+
+    card.style.position = 'absolute';
+    card.style.left = `${event.clientX - 200}px`;
+    card.style.top = `${event.clientY - 75}px`;
+}
